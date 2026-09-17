79e2f007221af33b42fbac6bb9d80dab5b84c1a4
Lucas Nicolás Ponte <lucasponteponte204@gmail.com>
2026-09-17 20:10:17 -0300

    pwa

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -13,6 +13,14 @@ import { ErrorMinlenght } from '../../components/ui/error-minlenght/error-minlen
   styleUrl: './login.css',
   templateUrl: './login.html',
 })
+//tema tps ahora lo visto traer vistos los tp para preguntas puntuales
+//pantalla que permita al usuario autenticarse anonimo o registrado
+//trabajar en orden del tp no es parte de la consigna la onda es hacer analisando todos los correos ordenar requerimiento basandome en resu final
+//ui pag que se paresca a otra no va 
+//revisar como se piden los asientos 
+//campos del registro van todos (indluso los que pide la consigna )revisar esto color de ojo va
+//no descartar nada de lo que esta en la consigna 
+//cambiar el logo y lo que aparece en la pantalla del tema lo de arriba de todo de google
 export class Login {
   logo = '/assets/imagenes/Gemini1r.png';
 
